ad48fe886ebee3d78f4a4346bbf093c127171864
Neal Fennimore <hi@neal.codes>
2020-02-26 22:44:36 -0500

    Initial commit

---
+++ babel.config.js
@@ -0,0 +1,7 @@
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+module.exports = ( api ) => {
+    api.cache( true );
+    return {
+        presets: ['babel-preset-expo'],
+    };
+};
