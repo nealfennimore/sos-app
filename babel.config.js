3a10125b641a027c19bdcd93ea9d82df459bc681
Neal Fennimore <hi@neal.codes>
2020-02-27 17:49:11 -0500

    Update aliasing for typescript and babel

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,24 @@
-/* eslint-disable @typescript-eslint/explicit-function-return-type */
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 module.exports = ( api ) => {
     api.cache( true );
     return {
         presets: [
             'babel-preset-expo',
+        ],
+        plugins: [
+            ['module-resolver', {
+                extensions: [
+                    '.js',
+                    '.jsx',
+                    '.ts',
+                    '.tsx',
+                    '.android.js',
+                    '.android.tsx',
+                    '.ios.js',
+                    '.ios.tsx',
+                ],
+                root: ['./src'],
+            }],
         ],
     };
 };
